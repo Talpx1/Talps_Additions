b6a7d18f8230d62459ae0185cf703ed7f0daceee
Talpx1 <simo3401@gmail.com>
2021-03-14 22:25:06 +0100

    Fixed AncientStatue model and placement + modified textures rendering for floreal vines

--- src/main/java/com/talp1/talpsadditions/block/AncientStatue.java
+++ src/main/java/com/talp1/talpsadditions/block/AncientStatue.java
@@ -3,12 +3,15 @@ package com.talp1.talpsadditions.block;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.item.BlockItemUseContext;
 import net.minecraft.state.properties.BlockStateProperties;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.shapes.ISelectionContext;
 import net.minecraft.util.math.shapes.VoxelShape;
 import net.minecraft.util.math.shapes.VoxelShapes;
 import net.minecraft.world.IBlockReader;
+
+import javax.annotation.Nullable;
 
 public class AncientStatue extends BasketBlock{
     private static final VoxelShape HORIZONTAL = Block.makeCuboidShape(5D, 0.0D, 5D, 11D, 8D, 11.0D);
@@ -35,5 +38,11 @@ public class AncientStatue extends BasketBlock{
             default:
                 return VoxelShapes.fullCube();
         }
+    }
+
+    @Nullable
+    @Override
+    public BlockState getStateForPlacement(BlockItemUseContext context) {
+        return getDefaultState().with(BlockStateProperties.HORIZONTAL_FACING, context.getPlacementHorizontalFacing().getOpposite());
     }
 }
--- src/main/java/com/talp1/talpsadditions/world/gen/VinesGen.java
+++ src/main/java/com/talp1/talpsadditions/world/gen/VinesGen.java
@@ -1,24 +1,16 @@
 package com.talp1.talpsadditions.world.gen;
 
 import com.talp1.talpsadditions.Main;
-import com.talp1.talpsadditions.utils.registration.ModBlocks;
 import com.talp1.talpsadditions.utils.registration.ModFeatures;
 import net.minecraft.world.biome.Biome;
 import net.minecraft.world.biome.Biomes;
 import net.minecraft.world.gen.GenerationStage;
-import net.minecraft.world.gen.blockplacer.DoublePlantBlockPlacer;
-import net.minecraft.world.gen.blockplacer.SimpleBlockPlacer;
-import net.minecraft.world.gen.blockstateprovider.SimpleBlockStateProvider;
 import net.minecraft.world.gen.feature.*;
-import net.minecraft.world.gen.feature.structure.IglooStructure;
-import net.minecraft.world.gen.placement.*;
-import net.minecraft.world.gen.treedecorator.TreeDecorator;
-import net.minecraft.world.gen.treedecorator.TreeDecoratorType;
 import net.minecraftforge.event.world.BiomeLoadingEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLLoadCompleteEvent;
-import net.minecraftforge.registries.ForgeRegistries;
+
+
 @Mod.EventBusSubscriber(modid = Main.MODID, bus = Mod.EventBusSubscriber.Bus.FORGE)
 public class VinesGen {
 
